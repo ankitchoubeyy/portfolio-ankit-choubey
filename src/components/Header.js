e583dba0b5f01a6169e9cf1a42d3f979ffe257f7
Ankit Choubey <imkitchoubey@gmail.com>
2025-11-04 11:09:23 +0530

    Header Component Added

---
+++ src/components/Header.js
@@ -0,0 +1,23 @@
+import Image from 'next/image'
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div className=' flex items-center justify-start gap-2 sm:gap-3'>
+            <div className="relative w-20 h-20">
+                <Image
+                    src="/profile.png"
+                    alt="logo"
+                    fill
+                    className="object-cover rounded-full"
+                />
+            </div>
+            <div className=''>
+                <h1 className='text-xl md:text-2xl font-bold text-white m-0'>Ankit Choubey</h1>
+                <p className='text-primary text-sm'>@ankitchoubeyy</p>
+            </div>
+        </div>
+    )
+}
+
+export default Header
